a45c633dca6ef1f0112ac5d158ac079d0a9dc361
raycakai <kaillainerayca@gmail.com>
2024-11-19 12:18:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'vl.feminino',
+    'qual medalha a seleçao ganhou em Pariz 2024?',
+    'medalha de bronze'
+)
+
+criaCartao(
+    'volei',
+    'desde quando o brasil tem sequencia historica de medalhas olimpicas?',
+    'desde 1992 o Brasil e medalhista olinpico no volei'
+)
+
+criaCartao(
+    'Ginastica',
+    'quando foi que Rebeca andrade ganhou seu orimeiro ouro olimpico?',
+    'Toquio 2020'
+)
+
+criaCartao(
+    'ginastica',
+    'Rebeca andrade venceu billes em qual categoria em pariz 2024?',
+    'solo'
+)
+
+criaCartao(
+    'Judo',
+    'Quem foi a medalhista de ouro no judo em Paris 2024?',
+    'bia souza'
+)
+
+criaCartao(
+    'volei de praia',
+    'nome da tupla campea olimpia no vlei de praia 2024?',
+    'Duda e Ana Patricia '
+    
+)
+
+criaCartao(
+    'volei de praia',
+    'com qual pais o Brasil jogou a final do jogos olimpicos no volei de praia ?',
+    'Canada'
+    
+)
+
+criaCartao(
+    'judo',
+    'qual modalidade o judo brasileiro ganhou pela primeira vez em paris?',
+    'assim como na ginastica o judo foi bronze por equie pela primeira vez em logos olimpicos '
+    
+)
